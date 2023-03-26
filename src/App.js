0a798f368b02df52515f9dda9bfb72a3cb44e9a0
deepu <deepuprajapati0846@gmail.com>
2023-03-26 16:15:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import Animalshow from './Animalshow';
+
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+    return animals[Math.floor(Math.random() * animals.length)];
+}
+console.log(getRandomAnimal());
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [animals, setAnimals] = useState([]);
+    const handleClick = () => {
+        setAnimals([...animals, getRandomAnimal()]);
+    };
+
+    const renderedAnimals = animals.map((animal, index) => {
+        return <Animalshow type={animal} key={index} />
+    });
+
+
+    //    function makeArray() {
+    //     return [1,10,30,23]
+    //    }
+
+    //    const myArray = makeArray();
+    // //    const firstElement = myArray[0];
+    // //    const secondElenet = myArray[1];
+    // // or
+    //    const [firstElement,secondElenet] = makeArray();
+    //    console.log(firstElement, secondElenet);
+
+    // const [count , setCount] = useState(0);
+    // const handleClick = () => {
+    //     setCount (count+1);
+    return (
+        <div className="app">
+            <button onClick={handleClick}> Add animals </button>
+            <div class="loader">
+                <div class="loading_1"></div>
+                <div class="loading_2">Loading GfG...</div>
+            </div>
+            <div className="animals-list"> {renderedAnimals}</div>
+        </div>
+    );
+
+};
+
+
+
 
 export default App;
